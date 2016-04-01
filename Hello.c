b5585475b39ce1746eca57e3992c2058e2b3e41e
mcumagic <mcumagic@163.com>
2016-04-01 10:25:42 +0800

    git tracks changes

--- Hello.c
+++ Hello.c
@@ -1,7 +1,8 @@
 void main()
 {
-	printf("Hello world!");
+	pintf("hello world!");
 	printf("you are bitch!");
 	printf("this is the third line code!");
 	printf("git has a mutable index called stageF!");
+	pirntf("git tracks changes!");
 }
